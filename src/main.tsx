3de3c2d131de6c5042c7dc91635baa67911949ad
Michael Bradvica <11015473@seaworld.com>
2025-02-20 10:50:56 -0500

    small updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import "../src/styles/index.scss";
 import { App } from "./app/app";
 import { Home } from "./features/home/home";
+import "../src/styles/index.scss";
 
 const root = createRoot(document.querySelector("#root") ?? new Element());
 
@@ -11,12 +11,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-    ],
+    children: [{ index: true, element: <Home /> }],
   },
 ]);
 
